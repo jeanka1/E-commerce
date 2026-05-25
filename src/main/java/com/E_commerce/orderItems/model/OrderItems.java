34f306c63737095884e9cb885fb5b3723f1372eb
Jean Carlos Sanchez <jeanka7043@gmail.com>
2026-05-25 09:39:53 -0500

    se creo la estructura del proyecto,y las clases model

---
+++ src/main/java/com/E_commerce/orderItems/model/OrderItems.java
@@ -0,0 +1,74 @@
+package com.E_commerce.orderItems.model;
+
+import java.util.UUID;
+
+public class OrderItems {
+
+    private UUID id;
+    private String name;
+    private double quantity;
+    private double price;
+    private double total;
+
+    public OrderItems() {
+    }
+
+    public OrderItems(UUID id, String name, double quantity, double price, double total) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.total = total;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItems{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", total=" + total +
+                '}';
+    }
+}
