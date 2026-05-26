6cdb3f8463baf0053ca4666c944071d06e2d37a4
Jean Carlos Sanchez <jeanka7043@gmail.com>
2026-05-26 18:02:43 -0500

    añadir los productos al carrito de compra y mostralos en la vista carrito

--- src/main/java/com/E_commerce/user/controller/UserController.java
+++ src/main/java/com/E_commerce/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.E_commerce.user.controller;
 
+import com.E_commerce.order.model.Order;
+import com.E_commerce.orderItems.model.OrderItems;
 import com.E_commerce.product.model.Product;
 import com.E_commerce.product.service.ProductService;
 import org.slf4j.Logger;
@@ -7,11 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -19,6 +20,13 @@ import java.util.Optional;
 public class UserController {
 
     private final Logger log= LoggerFactory.getLogger(UserController.class);
+
+    // para almacenar los detalles dela orden
+    List<OrderItems> items=new ArrayList<OrderItems>();
+
+    // va almacenar los datos de la oreden
+    Order order=new Order();
+
     @Autowired
     private ProductService productService;
 
@@ -39,7 +47,31 @@ public class UserController {
     }
 
     @PostMapping("/cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, Integer quantity, Model model){
+        OrderItems orderItems= new OrderItems();
+        Product product= new Product();
+        double sumaTotal=0;
+
+        Optional<Product> optionalProduct=productService.get(id);
+        log.info("producto añadido {}", optionalProduct.get());
+        log.info("cantidad {}", quantity);
+        product=optionalProduct.get();
+
+        orderItems.setQuantity(quantity);
+        orderItems.setPrice(product.getPrice());
+        orderItems.setName(product.getName());
+        orderItems.setTotal(product.getPrice()*quantity);
+        orderItems.setProduct(product);
+
+        items.add(orderItems);
+
+        sumaTotal=items.stream().mapToDouble(dt->dt.getTotal()).sum();
+
+        order.setTotal(sumaTotal);
+
+        model.addAttribute("cart",items);
+        model.addAttribute("order",order);
+
         return "user/cart";
     }
 }
