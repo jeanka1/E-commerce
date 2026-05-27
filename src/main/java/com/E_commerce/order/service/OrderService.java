7bb7430c6b501ae676899fcd666419f98454b778
Jean Carlos Sanchez <jeanka7043@gmail.com>
2026-05-27 10:09:15 -0500

    metodo para generar el numero de orden

--- src/main/java/com/E_commerce/order/service/OrderService.java
+++ src/main/java/com/E_commerce/order/service/OrderService.java
@@ -2,7 +2,10 @@ package com.E_commerce.order.service;
 
 import com.E_commerce.order.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
 
+    List<Order> findAll();
     Order save (Order order);
 }
--- src/main/java/com/E_commerce/order/service/OrderServiceImpl.java
+++ src/main/java/com/E_commerce/order/service/OrderServiceImpl.java
@@ -5,6 +5,9 @@ import com.E_commerce.order.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrderServiceImpl  implements OrderService{
 
@@ -15,4 +18,41 @@ public class OrderServiceImpl  implements OrderService{
     public Order save(Order order) {
         return orderRepository.save(order);
     }
+
+    @Override
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public String generarNumeroOrder(){
+
+        int number=0;
+        String numberConcatenado="";
+
+        List<Order> orders= findAll();
+        List<Integer> numbers= new ArrayList<Integer>();
+
+        orders.stream().forEach(o -> numbers.add( Integer.parseInt( o.getNumber()) ));
+
+        if(orders.isEmpty()){
+            number=1;
+        }else{
+            number=numbers.stream().max(Integer::compare).get();
+            number++;
+        }
+
+        if(number<10){
+            numberConcatenado="000000000"+String.valueOf(number);
+
+        }else if(number<100){
+            numberConcatenado="00000000"+String.valueOf(number);
+        }else if(number<1000){
+            numberConcatenado="0000000"+String.valueOf(number);
+        }else if(number<10000){
+            numberConcatenado="000000"+String.valueOf(number);
+        }
+
+        return numberConcatenado;
+    }
+
 }
