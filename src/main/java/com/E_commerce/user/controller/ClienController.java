19a083bbc675ca9f715bc6ec811b1fbdfa4f6b25
Jean Carlos Sanchez <jeanka7043@gmail.com>
2026-05-28 18:29:15 -0500

    mostrar la vista compras para el user

--- src/main/java/com/E_commerce/user/controller/ClienController.java
+++ src/main/java/com/E_commerce/user/controller/ClienController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,4 +62,10 @@ public class ClienController {
 
         return "redirect:/";
     }
+    @GetMapping("/compras")
+    public String obtenerCompras(Model model, HttpSession session){
+        model.addAttribute("session",session.getAttribute("iduser"));
+        return "user/compras";
+    }
+
 }
